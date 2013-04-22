53577fe0b2b754e2b19064cd47aa8ce0728fc26b
Etienne Lessard <elessard@avencall.com>
2013-04-22 10:38:11 -0400

    fix typo

--- xivo-libsccp/src/main.c
+++ xivo-libsccp/src/main.c
@@ -1087,7 +1087,7 @@ static char *sccp_resync_device(struct ast_cli_entry *e, int cmd, struct ast_cli
 		e->command = "sccp resync";
 		e->usage =
 			"Usage: sccp resync <device>\n"
-		        "       Resynchronize an SCCP device with it's updated configuration.\n";
+		        "       Resynchronize an SCCP device with its updated configuration.\n";
 		return NULL;
 
 	case CLI_GENERATE:
