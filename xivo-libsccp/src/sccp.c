2e631df5831664f7dc8ed2c721bd7031b99b9a78
Etienne Lessard <elessard@avencall.com>
2013-06-21 16:04:14 -0400

    conditional jump or move depends on uninitialised value
    Found using valgrind.

--- xivo-libsccp/src/sccp.c
+++ xivo-libsccp/src/sccp.c
@@ -435,11 +435,12 @@ static int handle_button_template_req_message(struct sccp_session *session)
 	int ret = 0;
 	struct sccp_msg *msg = NULL;
 	struct button_definition_template btl[42];
-	int button_count = 0;
+	int active_button_count = 0;
+	int device_button_count = 0;
 	uint32_t line_instance = 1;
 	struct sccp_line *line_itr = NULL;
 	struct sccp_speeddial *speeddial_itr = NULL;
-	uint8_t button_set = 0;
+	int button_set = 0;
 	int i = 0;
 
 	if (session == NULL) {
@@ -453,58 +454,52 @@ static int handle_button_template_req_message(struct sccp_session *session)
 		return -1;
 	}
 
-	ret = device_get_button_template(session->device, btl);
-	if (ret == -1)
+	device_button_count = device_get_button_count(session->device);
+	if (device_button_count == -1)
 		return -1;
 
-	for (i = 0; i < 42; i++) {
+	for (i = 0; i < device_button_count; i++) {
 		button_set = 0;
-		switch (btl[i].buttonDefinition) {
-			case BT_CUST_LINESPEEDDIAL:
 
-				msg->data.buttontemplate.definition[i].buttonDefinition = BT_NONE;
+		msg->data.buttontemplate.definition[i].buttonDefinition = BT_NONE;
+		msg->data.buttontemplate.definition[i].lineInstance = htolel(line_instance);
+
+		AST_RWLIST_RDLOCK(&session->device->lines);
+		AST_RWLIST_TRAVERSE(&session->device->lines, line_itr, list_per_device) {
+			if (line_itr->instance == line_instance) {
+				msg->data.buttontemplate.definition[i].buttonDefinition = BT_LINE;
 				msg->data.buttontemplate.definition[i].lineInstance = htolel(line_instance);
 
-				AST_RWLIST_RDLOCK(&session->device->lines);
-				AST_RWLIST_TRAVERSE(&session->device->lines, line_itr, list_per_device) {
-					if (line_itr->instance == line_instance) {
-						msg->data.buttontemplate.definition[i].buttonDefinition = BT_LINE;
-						msg->data.buttontemplate.definition[i].lineInstance = htolel(line_instance);
+				line_instance++;
+				active_button_count++;
+				button_set = 1;
+			}
+		}
+		AST_RWLIST_UNLOCK(&session->device->lines);
 
-						line_instance++;
-						button_count++;
-						button_set = 1;
-					}
+		if (button_set != 1) {
+			AST_RWLIST_RDLOCK(&session->device->speeddials);
+			AST_RWLIST_TRAVERSE(&session->device->speeddials, speeddial_itr, list_per_device) {
+				if (speeddial_itr->instance == line_instance) {
+					msg->data.buttontemplate.definition[i].buttonDefinition = STIMULUS_FEATUREBUTTON;
+					msg->data.buttontemplate.definition[i].lineInstance = htolel(line_instance);
+
+					line_instance++;
+					active_button_count++;
 				}
-				AST_RWLIST_UNLOCK(&session->device->lines);
-
-				if (button_set != 1) {
-					AST_RWLIST_RDLOCK(&session->device->speeddials);
-					AST_RWLIST_TRAVERSE(&session->device->speeddials, speeddial_itr, list_per_device) {
-						if (speeddial_itr->instance == line_instance) {
-							msg->data.buttontemplate.definition[i].buttonDefinition = STIMULUS_FEATUREBUTTON;
-							msg->data.buttontemplate.definition[i].lineInstance = htolel(line_instance);
-
-							line_instance++;
-							button_count++;
-						}
-					}
-					AST_RWLIST_UNLOCK(&session->device->speeddials);
-				}
-
-				break;
-
-			case BT_NONE:
-			default:
-				msg->data.buttontemplate.definition[i].buttonDefinition = BT_NONE;
-				msg->data.buttontemplate.definition[i].lineInstance = htolel(0);
-				break;
+			}
+			AST_RWLIST_UNLOCK(&session->device->speeddials);
 		}
 	}
 
+	for (; i < 42; i++) {
+		msg->data.buttontemplate.definition[i].buttonDefinition = BT_NONE;
+		msg->data.buttontemplate.definition[i].lineInstance = htolel(0);
+	}
+
 	msg->data.buttontemplate.buttonOffset = htolel(0);
-	msg->data.buttontemplate.buttonCount = htolel(button_count);
-	msg->data.buttontemplate.totalButtonCount = htolel(button_count);
+	msg->data.buttontemplate.buttonCount = htolel(active_button_count);
+	msg->data.buttontemplate.totalButtonCount = htolel(active_button_count);
 
 	ret = transmit_message(msg, session);
 	if (ret == -1)
