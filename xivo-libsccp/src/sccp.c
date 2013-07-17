30317f5b238dedf5c2b01bb537963885a0d2a111
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-07-17 10:50:55 -0400

    Revert "update condition in handle_onhook_message"
    This reverts commit 0949c07e3b85dc9d1a3e8129de679f3425213a25.

--- xivo-libsccp/src/sccp.c
+++ xivo-libsccp/src/sccp.c
@@ -1236,7 +1236,7 @@ static int handle_onhook_message(struct sccp_msg *msg, struct sccp_session *sess
 		return -1;
 	}
 
-	if (session->device->proto_version >= 11) {
+	if (session->device->proto_version == 11) {
 		/* Newest protocols provide these informations */
 
 		line_instance = msg->data.onhook.lineInstance;
