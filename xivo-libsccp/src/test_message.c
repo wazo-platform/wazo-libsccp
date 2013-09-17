f3866736d4f53f78b840b4bc6eab14b45c543bb6
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-09-17 10:54:27 -0400

    Use assert_null_handled in sccp_test_null_arguments

--- xivo-libsccp/src/test_message.c
+++ xivo-libsccp/src/test_message.c
@@ -139,9 +139,7 @@ cleanup:
 
 AST_TEST_DEFINE(sccp_test_null_arguments)
 {
-	int ret = 0;
 	enum ast_test_result_state result = AST_TEST_PASS;
-	void *retptr = NULL;
 
 	switch (cmd) {
 	case TEST_INIT:
@@ -158,15 +156,14 @@ AST_TEST_DEFINE(sccp_test_null_arguments)
 
 	ast_test_status_update(test, "Executing sccp test null arguments...\n");
 
+	/* returns void will segfault if it fails */
+	speeddial_hints_subscribe(NULL, NULL);
+
 	assert_null_handled(device_get_speeddial(NULL, 0), NULL);
 	assert_null_handled(device_get_speeddial_by_index(NULL, 0), NULL);
 	assert_null_handled(transmit_feature_status(NULL, 0, 0, 0, ""), -1);
 	assert_null_handled(transmit_feature_status((struct sccp_session *)0x1, 0, 0, 0, NULL), -1);
 	assert_null_handled(speeddial_hints_cb("", "", 0, NULL), -1);
-
-	/* returns void will segfault if it fails */
-	speeddial_hints_subscribe(NULL, NULL);
-
 	assert_null_handled(handle_feature_status_req_message(NULL, (struct sccp_session *)0x1), -1);
 	assert_null_handled(handle_feature_status_req_message((struct sccp_msg *)0x1, NULL), -1);
 	assert_null_handled(handle_speeddial_message(NULL, (struct sccp_session *)0x1), -1);
@@ -182,292 +179,47 @@ AST_TEST_DEFINE(sccp_test_null_arguments)
 	assert_null_handled(register_device((void*)0xFF, NULL), -1);
 	assert_null_handled(sccp_new_channel(NULL, (void*)0xFF), NULL);
 	assert_null_handled(cb_ast_get_rtp_peer(NULL, (void*)0xFF), AST_RTP_GLUE_RESULT_FORBID);
-
-	if (cb_ast_get_rtp_peer((void*)0xFF, NULL) != AST_RTP_GLUE_RESULT_FORBID) {
-		ast_test_status_update(test, "failed: cb_ast_get_rtp_peer(0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = start_rtp(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: start_rtp(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = sccp_start_the_call(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: sccp_start_the_call(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = sccp_lookup_exten(NULL);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: sccp_lookup_exten(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_offhook_message(NULL, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_offhook_message(NULL, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_onhook_message(NULL, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_onhook_message(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_hold(0, 0, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_hold(0, 0, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_resume(0, 0, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_resume(0, 0, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_transfer(0, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_transfer(0, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_event_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_event_message(NULL, 0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_event_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_event_message(0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_softkey_set_req_message(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_softkey_set_req_message(NULL, 0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = codec_sccp2ast(0, NULL);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: codec_sccp2ast(0, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_capabilities_res_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_capabilities_res_message(NULL, 0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_capabilities_res_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_capabilities_res_message(0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_open_receive_channel_ack_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_open_receive_channel_ack_message(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_open_receive_channel_ack_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_open_receive_channel_ack_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_line_status_req_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_line_status_req_message(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_line_status_req_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_line_status_req_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_register_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_register_message(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_register_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_register_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_ipport_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_ipport_message(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_ipport_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_ipport_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_keypad_button_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_ipport_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_keypad_button_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_keypad_button_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_message(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_message(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = handle_message((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: handle_message((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = fetch_data(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: fetch_data(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = cb_ast_request(NULL, (void*)0xFF, (void*)0xFF, (void*)0xFF, (void*)0xFF);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: cb_ast_request(NULL, 0xFF, (void*)0xFF, (void*)0xFF, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = cb_ast_request((void*)0xFF, (void*)0xFF, (void*)0xFF, NULL, (void*)0xFF);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: cb_ast_request((void*)0xFF, 0xFF, NULL, (void*)0xFF, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = cb_ast_request((void*)0xFF, (void*)0xFF, (void*)0xFF, (void*)0xFF, NULL);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: cb_ast_request((void*)0xFF, 0xFF, (void*)0xFF, (void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_call(NULL, (void*)0xFF, 0);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_call(NULL, (void*)0xFF, 0)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_call((void*)0xFF, NULL, 0);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_call((void*)0xFF, NULL, 0)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_hangup(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_hangup(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_answer(NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_answer(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	retptr = cb_ast_read(NULL);
-	if (retptr != NULL) {
-		ast_test_status_update(test, "failed: cb_ast_read(NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_write(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_write(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_write((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_write((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_indicate(NULL, 0xFF, (void*)0xFF, 0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_indicate(NULL, 0xFF, (void*)0xFF, 0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_fixup(NULL, (void*)0xFF);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_fixup(NULL, (void*)0xFF)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
-
-	ret = cb_ast_fixup((void*)0xFF, NULL);
-	if (ret != -1) {
-		ast_test_status_update(test, "failed: cb_ast_fixup((void*)0xFF, NULL)\n");
-		result = AST_TEST_FAIL;
-		goto cleanup;
-	}
+	assert_null_handled(cb_ast_get_rtp_peer((void*)0xFF, NULL), AST_RTP_GLUE_RESULT_FORBID);
+	assert_null_handled(start_rtp(NULL), -1);
+	assert_null_handled(sccp_start_the_call(NULL), -1);
+	assert_null_handled(sccp_lookup_exten(NULL), NULL);
+	assert_null_handled(handle_offhook_message(NULL, NULL), -1);
+	assert_null_handled(handle_onhook_message(NULL, NULL), -1);
+	assert_null_handled(handle_softkey_hold(0, 0, NULL), -1);
+	assert_null_handled(handle_softkey_resume(0, 0, NULL), -1);
+	assert_null_handled(handle_softkey_transfer(0, NULL), -1);
+	assert_null_handled(handle_softkey_event_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_softkey_event_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_softkey_set_req_message(NULL), -1);
+	assert_null_handled(codec_sccp2ast(0, NULL), NULL);
+	assert_null_handled(handle_capabilities_res_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_capabilities_res_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_open_receive_channel_ack_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_open_receive_channel_ack_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_line_status_req_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_line_status_req_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_register_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_register_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_ipport_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_ipport_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_keypad_button_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_keypad_button_message((void*)0xFF, NULL), -1);
+	assert_null_handled(handle_message(NULL, (void*)0xFF), -1);
+	assert_null_handled(handle_message((void*)0xFF, NULL), -1);
+	assert_null_handled(fetch_data(NULL), -1);
+	assert_null_handled(cb_ast_request(NULL, (void*)0xFF, (void*)0xFF, (void*)0xFF, (void*)0xFF), NULL);
+	assert_null_handled(cb_ast_request((void*)0xFF, (void*)0xFF, (void*)0xFF, NULL, (void*)0xFF), NULL);
+	assert_null_handled(cb_ast_request((void*)0xFF, (void*)0xFF, (void*)0xFF, (void*)0xFF, NULL), NULL);
+	assert_null_handled(cb_ast_call(NULL, (void*)0xFF, 0), -1);
+	assert_null_handled(cb_ast_call((void*)0xFF, NULL, 0), -1);
+	assert_null_handled(cb_ast_hangup(NULL), -1);
+	assert_null_handled(cb_ast_answer(NULL), -1);
+	assert_null_handled(cb_ast_read(NULL), NULL);
+	assert_null_handled(cb_ast_write(NULL, (void*)0xFF), -1);
+	assert_null_handled(cb_ast_write((void*)0xFF, NULL), -1);
+	assert_null_handled(cb_ast_indicate(NULL, 0xFF, (void*)0xFF, 0xFF), -1);
+	assert_null_handled(cb_ast_fixup(NULL, (void*)0xFF), -1);
+	assert_null_handled(cb_ast_fixup((void*)0xFF, NULL), -1);
 
 cleanup:
 	return result;
