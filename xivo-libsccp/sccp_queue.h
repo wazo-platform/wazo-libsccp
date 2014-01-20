648f4d5a23081ff6d9a91e1ad3215f5469b9f12d
Etienne Lessard <elessard@avencall.com>
2014-01-20 13:43:35 -0500

    rework sccp_queue doc

--- xivo-libsccp/sccp_queue.h
+++ xivo-libsccp/sccp_queue.h
@@ -12,45 +12,38 @@ typedef void (*sccp_queue_process_cb)(void *msg_data, void *arg);
 #define SCCP_QUEUE_EMPTY 2
 #define SCCP_QUEUE_ERROR 3
 
-/*
- * Create a new (FIFO) queue, with msg that can be sent with a maximum msg size
- * of msg_size.
+/*!
+ * \brief Create a new (FIFO) queue.
+ *
+ * \param msg_size size of message data
+ *
+ * \retval non-NULL on success
+ * \retval NULL on failure
  */
 struct sccp_queue *sccp_queue_create(size_t msg_size);
 
-/*
- * destroy the queue
- *
- * Note that if there's message remaining that hold resources,
- * this will cause a leak.
+/*!
+ * \brief Destroy the queue.
  */
 void sccp_queue_destroy(struct sccp_queue *queue);
 
-/*
- * Return the read file descriptor of the queue.
+/*!
+ * \brief Return the read file descriptor of the queue.
  *
- * The file descriptor is ready when the queue is not empty, and it is
- * not ready when the queue is empty.
- *
- * No read/write/close must be done on the fd; it MUST
- * only be used in a select / poll statement to detect when there
- * is msg to be read from the queue.
+ * \note The file descriptor is ready when the queue is not empty.
+ * \note You must not use the file descriptor for anything else than select / poll.
  */
 int sccp_queue_fd(struct sccp_queue *queue);
 
 /*!
- * \brief Close the queue so that sccp_queue_put will return an error.
+ * \brief Close the queue so that no more message can be queued.
  *
  * \note This does not destroy the queue.
- *
- * XXX maybe rename to sccp_queue_shutdown ?
  */
 void sccp_queue_close(struct sccp_queue *queue);
 
 /*!
- * \brief Put a message to the queue
- *
- * \note The message is copied from *msg_data.
+ * \brief Put a message into the queue.
  *
  * \retval 0 on success
  * \retval SCCP_QUEUE_CLOSED if the queue is closed
@@ -61,8 +54,6 @@ int sccp_queue_put(struct sccp_queue *queue, void *msg_data);
 /*!
  * \brief Get the next message from the queue.
  *
- * \note The message is copied to *msg_data.
- *
  * \retval 0 on success
  * \retval SCCP_QUEUE_EMPTY if the queue is empty
  */
@@ -71,10 +62,8 @@ int sccp_queue_get(struct sccp_queue *queue, void *msg_data);
 /*
  * \brief Process all the messages from the queue.
  *
- * \note In theory, should be more efficient than getting one message at
- *       a time.
- * \note The callback is called while the queue's lock is not held, so don't
- *       be afraid of deadlock in the callback
+ * \note In theory, is a bit more efficient than getting one message at a time.
+ * \note No lock is held while the callback is called.
  */
 void sccp_queue_process(struct sccp_queue *queue, sccp_queue_process_cb callback, void *arg);
 
